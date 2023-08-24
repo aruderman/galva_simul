d29ae7c78a9ec8f971d5b09c84c1e03f9eca58e6
aruderman <casilla2222@gmail.com>
2023-08-24 15:23:12 -0300

    simulation_setup class

---
+++ galva_diagrams/simulation_setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of galva_simul (https://github.com/aruderman/galva_simul/).
+# Copyright (c) 2021-2023, Maximiliano Gavilán y Andrés Ruderman
+# License: MIT
+#   Full Text: https://github.com/aruderman/galva_simul/blob/master/LICENSE
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# DOCS
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+"""simulation_setup class of galva_simul."""
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# IMPORTS
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# CLASSES
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+class SimulationSetup:
+	def __init__(self,
+		Npx : int,
+    	Npt : int,
+    	NPOINTS : int,
+        D : float,
+    	ks : float,
+    	T : float,
+    	Mr : float,
+    	m : float,
+    	d : float,
+    	rho : float,
+    	Rohm : float,
+    	Xi0 : float,
+    	Xif : float,
+    	NXi : int,
+    	L0 : float,
+    	Lf : float,
+    	NL : int,
+    	Eoff : float
+    	):
+
+		self.Npx = Npx
+    	self.Npt = Npt
+    	self.NPOINTS = NPOINTS
+    	self.D = D
+    	self.ks = ks
+    	self.T = T
+    	self.Mr = Mr
+    	self.m = m
+    	self.d = d
+    	self.rho = rho
+    	self.Rohm = Rohm
+    	self.Xi0 = Xi0
+    	self.Xif = Xif
+    	self.NXi = NXi
+    	self.L0 = L0
+    	self.Lf = Lf
+    	self.NL = NL
+    	self.Eoff = Eoff
+
