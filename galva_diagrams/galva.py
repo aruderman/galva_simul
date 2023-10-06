e7aac3f2f47b97948a3ccfaa991a42f7c32280b4
aruderman <casilla2222@gmail.com>
2023-10-06 16:37:05 -0300

    fixed galva.py

--- galva_diagrams/galva.py
+++ galva_diagrams/galva.py
@@ -23,135 +23,161 @@ import ctypes as ct
 # CLASSES
 # ============================================================================
 
+
 class galva_diagram:
-	def __init__(self, 
-		Npx, 
-		Npt, 
-		NPOINTS, 
-		Niso, 
-		Xif, 
-		Xi0, 
-		N
-		Xi, 
-		Lf, 
-		L0,  
-		NL, 
-		D, 
-		ks, 
-		T, 
-		Mr, 
-		m, 
-		rho, 
-		Rohm, 
-		Eoff, 
-		ai, 
-		bi, 
-		ci, 
-		di, 
-		theta_inter):
+    def __init__(
+        self,
+        so_path,
+        Npx,
+        Npt,
+        NPOINTS,
+        Niso,
+        Xif,
+        Xi0,
+        NXi,
+        Lf,
+        L0,
+        NL,
+        D,
+        ks,
+        T,
+        Mr,
+        m,
+        rho,
+        Rohm,
+        Eoff,
+        Qmax,
+        ai,
+        bi,
+        ci,
+        di,
+        capacidad,
+    ):
+        self.so_path = so_path
+        self.Npx = Npx
+        self.Npt = Npt
+        self.NPOINTS = NPOINTS
+        self.Niso = Niso
+        self.Xif = Xif
+        self.Xi0 = Xi0
+        self.NXi = NXi
+        self.Lf = Lf
+        self.L0 = L0
+        self.NL = NL
+        self.D = D
+        self.ks = ks
+        self.T = T
+        self.Mr = Mr
+        self.m = m
+        self.rho = rho
+        self.Rohm = Rohm
+        self.Eoff = Eoff
+        self.Qmax = Qmax
+        self.ai = ai
+        self.bi = bi
+        self.ci = ci
+        self.di = di
+        self.capacidad = capacidad
 
-		self.Npx = NPx
-		self.Npt = Npt
-		self.NPOINTS = NPOINTS
-		self.Niso = Niso
-		self.Xif = Xif 
-		self.Xi0 = Xi0
-		self.NXi = NXi
-		self.Lf = Lf
-		self.L0 = L0
-		self.NL = NL
-		self.D = D
-		self.ks = ks
-		self.T = T
-		self.Mr = Mr
-		self.m = m
-		self.rho = rho
-		self.Rohm = Rohm
-		self.Eoff = Eoff
-		self.ai = ai
-		self.bi = bi
-		self.ci = ci 
-		self.di = di 
-		self.theta_inter = theta_inter
-		
+    def simulation_params(self):
+        return (
+            self.Npx,
+            self.Npt,
+            self.NPOINTS,
+            self.Niso,
+            self.Xif,
+            self.Xi0,
+            self.NXi,
+            self.Lf,
+            self.L0,
+            self.NL,
+            self.D,
+            self.ks,
+            self.T,
+            self.Mr,
+            self.m,
+            self.rho,
+            self.Rohm,
+            self.Eoff,
+            self.Qmax,
+        )
 
-	def galva_calc(self):
+    def galva_calc(self):
 
-	    lib_galva = ct.CDLL("./galva_parallel.so")
-	    
-	    lib_galva.galva.argtypes = [
-	        ct.c_int,
-	        ct.c_int,
-	        ct.c_int,
-	        ct.c_int,
-	        ct.c_double,
-	        ct.c_double,
-	        ct.c_int,
-	        ct.c_double,
-	        ct.c_double,
-	        ct.c_int,
-	        ct.c_double,
-	        ct.c_double,
-	        ct.c_double,
-	        ct.c_double,
-	        ct.c_double,
-	        ct.c_double,
-	        ct.c_double,
-	        ct.c_double,
-	        ct.POINTER(ct.c_double),
-	        ct.POINTER(ct.c_double),
-	        ct.POINTER(ct.c_double),
-	        ct.POINTER(ct.c_double),
-	        ct.POINTER(ct.c_double),
-	        ct.POINTER(ct.c_double),
-	        ct.POINTER(ct.c_double),
-	        ct.POINTER(ct.c_double),
-	        ]
+        path = self.so_path
 
-	    N = self.NL * self.NXi
+        lib_galva = ct.CDLL(path)
 
-	    logL = (ct.c_double * N)()
-	    logXi = (ct.c_double * N)()
-	    SOC = (ct.c_double * N)()
+        lib_galva.galva.argtypes = [
+            ct.c_int,
+            ct.c_int,
+            ct.c_int,
+            ct.c_int,
+            ct.c_double,
+            ct.c_double,
+            ct.c_int,
+            ct.c_double,
+            ct.c_double,
+            ct.c_int,
+            ct.c_double,
+            ct.c_double,
+            ct.c_double,
+            ct.c_double,
+            ct.c_double,
+            ct.c_double,
+            ct.c_double,
+            ct.c_double,
+            ct.c_double,
+            ct.POINTER(ct.c_double),
+            ct.POINTER(ct.c_double),
+            ct.POINTER(ct.c_double),
+            ct.POINTER(ct.c_double),
+            ct.POINTER(ct.c_double),
+            ct.POINTER(ct.c_double),
+            ct.POINTER(ct.c_double),
+            ct.POINTER(ct.c_double),
+        ]
 
-	    lib_galva.galva(
-	        self.Npx, 
-	        self.Npt, 
-	        self.NPOINTS, 
-	        self.Niso, 
-	        self.Xif, 
-	        self.Xi0, 
-	        self.NXi, 
-	        self.Lf, 
-	        self.L0,  
-	        self.NL,
-	        self.D, 
-	        self.ks, 
-	        self.T, 
-	        self.Mr, 
-	        self.m, 
-	        self.rho, 
-	        self.Rohm, 
-	        self.Eoff, 
-	        self.ai.ctypes.data_as(ct.POINTER(ct.c_double)),
-	        self.bi.ctypes.data_as(ct.POINTER(ct.c_double)),
-	        self.ci.ctypes.data_as(ct.POINTER(ct.c_double)),
-	        self.di.ctypes.data_as(ct.POINTER(ct.c_double)),
-	        self.theta_inter.ctypes.data_as(ct.POINTER(ct.c_double)),
-	        logL,
-	        logXi, 
-	        SOC,   
-	        )
-	    
-	    logL = np.asarray(
-	        np.frombuffer(logL, dtype=np.double, count=N)
-	    )
-	    logXi = np.asarray(
-	        np.frombuffer(logXi, dtype=np.double, count=N)
-	    )
+        N = int(self.NL * self.NXi)
 
-	    SOC = np.asarray(
-	        np.frombuffer(SOC, dtype=np.double, count=N)
-	    )
-	    
-	    return logL, logXi, SOC
+        res1 = (ct.c_double * N)()
+        res2 = (ct.c_double * N)()
+        res3 = (ct.c_double * N)()
+
+        lib_galva.galva(
+            self.Npx,
+            self.Npt,
+            self.NPOINTS,
+            self.Niso,
+            self.Xif,
+            self.Xi0,
+            self.NXi,
+            self.Lf,
+            self.L0,
+            self.NL,
+            self.D,
+            self.ks,
+            self.T,
+            self.Mr,
+            self.m,
+            self.rho,
+            self.Rohm,
+            self.Eoff,
+            self.Qmax,
+            self.ai.ctypes.data_as(ct.POINTER(ct.c_double)),
+            self.bi.ctypes.data_as(ct.POINTER(ct.c_double)),
+            self.ci.ctypes.data_as(ct.POINTER(ct.c_double)),
+            self.di.ctypes.data_as(ct.POINTER(ct.c_double)),
+            self.capacidad.ctypes.data_as(ct.POINTER(ct.c_double)),
+            res1,
+            res2,
+            res3,
+        )
+
+        logL = np.asarray(np.frombuffer(res1, dtype=np.double, count=N))
+
+        logXi = np.asarray(np.frombuffer(res2, dtype=np.double, count=N))
+
+        SOC = np.asarray(np.frombuffer(res3, dtype=np.double, count=N))
+
+        return logL, logXi, SOC
