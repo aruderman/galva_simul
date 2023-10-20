dc562b6fae7a44c6f7c47169465bd5a65659a546
aruderman <casilla2222@gmail.com>
2023-10-20 15:52:07 -0300

    corrected issues on PIBB

--- galva_diagrams/lib/galva_PIBB.cpp
+++ galva_diagrams/lib/galva_PIBB.cpp
@@ -22,10 +22,10 @@
 extern "C" void galva(int Npx, int Npt, int NPOINT, int Niso, double Xif, double Xi0, int NXi,
                       double Lf, double L0, int NL, double D, double ks, double T, double Mr,
                       double m, double rho, double Rohm, double Eoff, double Qmax, double geo,
-                      double *ai, double *bi, double *ci, double *di, double *titaeq, 
-                      double *res1, double *res2, double *res3) {
+                      double *ai, double *bi, double *ci, double *di, double *titaeq, double *res1,
+                      double *res2, double *res3) {
 
- // FILE *archivo;
+  // FILE *archivo;
 
   // /// In-put
   // #define lee_info_en "isoterma-csaps-galva6.dat"
@@ -36,8 +36,8 @@ extern "C" void galva(int Npx, int Npt, int NPOINT, int Niso, double Xif, double
   // /// STEP--------------------------------------------------------------------------------------
   // // Generation of output file title
   //(archivo = fopen("Profile-Out.dat", "a"));
-  //fprintf(archivo, "# SOC Potential[V]\n");
-  //fclose(archivo);
+  // fprintf(archivo, "# SOC Potential[V]\n");
+  // fclose(archivo);
 
   // Defining simulation parameters
   // int 		omp_get_num_threads(void);
@@ -80,22 +80,21 @@ extern "C" void galva(int Npx, int Npt, int NPOINT, int Niso, double Xif, double
         /// Actualization of the parameters
         double L = pow(10, logL[EL]);
         double Xi = pow(10, logXi[XI]);
-        double Cr = (ks / Xi) * (ks / Xi) * (th / D);   /// C-rate
+        double Cr = (ks / Xi) * (ks / Xi) * (th / D);         /// C-rate
         double d = 2.0 * sqrt((L * (1 + geo) * D * th) / Cr); /// particle diameter, cm
-        double S = 2.0 * (1 + geo) * m / (rho * d); /// Surface area, cm2
+        double S = 2.0 * (1 + geo) * m / (rho * d);           /// Surface area, cm2
         // double	Vol=m/rho; 						      	///Volume of
         // active mass, cm3
-        double ic = - Cr * Qmax * m / (1000 * S); /// constant current density, A/cm2
-        double iR = Rohm * ic * S;                         /// IR drop, A*ohm=V
+        double ic = -Cr * Qmax * m / (1000 * S); /// constant current density, A/cm2
+        double iR = Rohm * ic * S;               /// IR drop, A*ohm=V
         double c1 = rho / Mr;
         double iN = 1.0 / (F * D * c1);
-      //  double ttot = 0.5 * 0.5 * d * (rho / Mr) * F / (-ic); /// total time, s CHEQUEAAARRRR
+        //  double ttot = 0.5 * 0.5 * d * (rho / Mr) * F / (-ic); /// total time, s CHEQUEAAARRRR
         double ttot = abs(Qmax * m * 3.6 / (ic * S));
         double NT = Npt;
         double NX = Npx;
-        double Dt = ttot / (NT - 1.0);                       /// time step, s
-        double Dd = 0.5 * d / (NX - 1.0);                    /// space step, cm
-        
+        double Dt = ttot / (NT - 1.0);    /// time step, s
+        double Dd = 0.5 * d / (NX - 1.0); /// space step, cm
 
         // Cleaning vectors
         double betaT[Npx], alfaT[Npx], bN[Npx], tita0[Npx], tita1[Npx];
@@ -122,7 +121,7 @@ extern "C" void galva(int Npx, int Npt, int NPOINT, int Niso, double Xif, double
         /// Initial Point
         double ti = 0.0;
         for (int i = 0; i < Npx; i++) {
-          tita1[i] = titaeq[1];
+          tita1[i] = 1e-5;
         }
         double Ei = Eoff + 1.0; // any value just that Ei>Eoff
         double E0 = 0.0;
@@ -131,63 +130,74 @@ extern "C" void galva(int Npx, int Npt, int NPOINT, int Niso, double Xif, double
         int TP = 0;
 
         /// TIME LOOP------------------------------------------------------------------------
-        //while( (Ei >= Eoff) && (Npot < Niso - 1)) {
+        // while( (Ei >= Eoff) && (Npot < Niso - 1)) {
 
-
-        while(Ei>Eoff){
-        ///POTENTIAL CALCULATION STEP
-          //Search range of experimental points where superficial concentration (tita1) belongs
-            double Ai,Bi,Ci,Di,titad;
-            for(int i=0;i<Niso;i++){
-              if((tita1[Npx-1]>=titaeq[i])&&(tita1[Npx-1]<titaeq[i+1])){
-                Ai=ai[i];Bi=bi[i];Ci=ci[i];Di=di[i];titad=titaeq[i];
-                break;
-              }
+        while (Ei > Eoff) {
+          /// POTENTIAL CALCULATION STEP
+          // Search range of experimental points where superficial concentration (tita1) belongs
+          double Ai, Bi, Ci, Di, titad;
+          for (int i = 0; i < Niso; i++) {
+            if ((tita1[Npx - 1] >= titaeq[i]) && (tita1[Npx - 1] < titaeq[i + 1])) {
+              Ai = ai[i];
+              Bi = bi[i];
+              Ci = ci[i];
+              Di = di[i];
+              titad = titaeq[i];
+              break;
             }
-            double dtitas = tita1[Npx-1] - titad;
-
-          //Equilibrium potential calculation
-          double  E0 = Ai+Bi*dtitas+Ci*dtitas*dtitas+Di*dtitas*dtitas*dtitas;
-          double  i0 = F*c1*ks*sqrt(tita1[Npx-1]*(1.0-tita1[Npx-1]));
-          //Potential calculation
-          Ei = E0 + 2.0*f*asinh(ic/(2.0*i0));
-          //printf("dtit=%f Ai=%f Bi=%f Ci=%f Di=%f E0=%f i0=%f Ei=%f",dtitas, Ai, Bi, Ci, Di, E0, i0, Ei);
-      
-        ///PRINT POTENTIAL PROFILE POINT
-          if(TP%NMOD==0){
-            double SOC=0.0;
-            for(int i=0;i<Npx;i++){SOC+=tita1[i];}
-            SOC/=(NX-1);  
-            
-          //(archivo = fopen("Profile-Out.dat", "a"));
-          //fprintf(archivo, "%f %f\n", (float)(SOC), (float)(Ei));
-          //fclose(archivo);
           }
-          
-        ///ACTUALIZATION STEP
-          for(int i=0;i<Npx;i++){
-            tita0[i]=tita1[i];
-            betaT[i]=bN[i]=tita1[i]=0.0;
+          double dtitas = tita1[Npx - 1] - titad;
+
+          // Equilibrium potential calculation
+          //double E0 = Ai + Bi * dtitas + Ci * dtitas * dtitas + Di * dtitas * dtitas * dtitas;
+          double E0 = 0;
+          double i0 = F * c1 * ks * sqrt(tita1[Npx - 1] * (1.0 - tita1[Npx - 1]));
+          // Potential calculation
+          Ei = E0 + 2.0 * f * asinh(ic / (2.0 * i0));
+          // printf("dtit=%f Ai=%f Bi=%f Ci=%f Di=%f E0=%f i0=%f Ei=%f",dtitas, Ai, Bi, Ci, Di, E0,
+          // i0, Ei);
+
+          /// PRINT POTENTIAL PROFILE POINT
+          if (TP % NMOD == 0) {
+            double SOC = 0.0;
+            for (int i = 0; i < Npx; i++) {
+              SOC += tita1[i];
+            }
+            SOC /= (NX - 1);
+
+            //(archivo = fopen("Profile-Out.dat", "a"));
+            // fprintf(archivo, "%f %f\n", (float)(SOC), (float)(Ei));
+            // fclose(archivo);
           }
 
-          //Vector of solutions and Thomas coefficients calculation
-            bN[0]=tita0[0];
-            bN[Npx-1]=tita0[Npx-1]-((Abi+(Bbi/r[Npx-1]))*2.0*Dd*(ic*iN));
-            for(int i=1;i<Npx-1;i++){bN[i]=tita0[i];}
+          /// ACTUALIZATION STEP
+          for (int i = 0; i < Npx; i++) {
+            tita0[i] = tita1[i];
+            betaT[i] = bN[i] = tita1[i] = 0.0;
+          }
 
-            betaT[1]=bN[0]/A0bi;
-            for(int i=2;i<Npx;i++){
-                betaT[i]=(bN[i-1]+((Abi-(Bbi/(r[i-1])))*betaT[i-1]))/(A0bi-(Abi-(Bbi/(r[i-1])))*alfaT[i-1]);
-            }
+          // Vector of solutions and Thomas coefficients calculation
+          bN[0] = tita0[0];
+          bN[Npx - 1] = tita0[Npx - 1] - ((Abi + (Bbi / r[Npx - 1])) * 2.0 * Dd * (ic * iN));
+          for (int i = 1; i < Npx - 1; i++) {
+            bN[i] = tita0[i];
+          }
 
-          //Concentration calculation
-            tita1[Npx-1]=(bN[Npx-1]+2.0*Abi*betaT[Npx-1])/(A0bi-2.0*Abi*alfaT[Npx-1]);
-            for(int i=2;i<Npx+1;i++){
-              tita1[Npx-i]=(alfaT[Npx-(i-1)]*tita1[Npx-(i-1)])+betaT[Npx-(i-1)];
-            }
-        ti+=Dt;TP++;    ///time increment
+          betaT[1] = bN[0] / A0bi;
+          for (int i = 2; i < Npx; i++) {
+            betaT[i] = (bN[i - 1] + ((Abi - (Bbi / (r[i - 1]))) * betaT[i - 1])) /
+                       (A0bi - (Abi - (Bbi / (r[i - 1]))) * alfaT[i - 1]);
+          }
+
+          // Concentration calculation
+          tita1[Npx - 1] =
+              (bN[Npx - 1] + 2.0 * Abi * betaT[Npx - 1]) / (A0bi - 2.0 * Abi * alfaT[Npx - 1]);
+          for (int i = 2; i < Npx + 1; i++) {
+            tita1[Npx - i] = (alfaT[Npx - (i - 1)] * tita1[Npx - (i - 1)]) + betaT[Npx - (i - 1)];
+          }
+          ti += Dt;
+          TP++; /// time increment
         }
-
 
         /// PRINT POTENTIAL PROFILE POINT AFTER WHILE LOOP ENDS
         double SOC = 0.0;
