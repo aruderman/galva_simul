e4cb986548e599405a74fdf289ef44440ff97356
aruderman <casilla2222@gmail.com>
2023-12-01 14:57:42 -0300

    added gitignore and init

---
+++ galva_diagrams/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of galva_simul (https://github.com/aruderman/galva_simul/).
+# Copyright (c) 2022-2023, Maximiliano Gavilan and Andrés Ruderman
+# License: MIT
+#   Full Text: https://github.com/aruderman/galva_simul/blob/master/LICENSE
+
+# ============================================================================
+# DOCS
+# ============================================================================
+
+"""A Python library with C++ extensions to make diagrams."""
+
+# ============================================================================
+# CONSTANTS
+# ============================================================================
+
+__author__ = """Maximiliano Gavilan and Andrés Ruderman"""
+__email__ = "andres.ruderman@gmail.com"
+__version__ = "0.1.1"
+
+
+# ============================================================================
+# IMPORTS
+# ============================================================================
+
+# spline
+from .spline import SplineParams  # noqa
+
+# diagram calculations
+from .Simulation import GalvanostaticMap, GalvanostaticProfile  # noqa
